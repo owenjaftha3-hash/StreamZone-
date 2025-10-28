1ec03a69220fcdc9cfdc78284aefe3c24b7aefa6
owenjaftha3-hash <owenjaftha3@gmail.com>
2025-10-28 02:04:39 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'media.tenor.com', 'i.imgur.com', 'cdn.pixabay.com', 'supabase.co'],
-  },
-};
-
-module.exports = nextConfig;
