ad0a9589658437c387bb10dd9e831d9feb22f7f4
owenjaftha3-hash <owenjaftha3@gmail.com>
2025-10-28 02:13:06 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import MovieList from "../components/MovieList";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <h1>🎬 StreamZone</h1>
+      <MovieList />
+    </main>
+  );
+}
